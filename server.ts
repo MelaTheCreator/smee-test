f9616cb19e6a93488c6356e45a2f1c6cd8bbdda2
Mela Heß <melanie.hess.74@dci-student.org>
2026-06-02 12:03:26 +0200

    task: add git workflow

---
+++ server.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import type { Request, Response, NextFunction } from "express";
+import { Webhooks, createNodeMiddleware } from "@octokit/webhooks";
+
+const secret = process.env.WEBHOOK_SECRET || null;
+if (!secret) {
+  console.error("secret is not defined.");
+  process.exit(1);
+}
+const webhooks = new Webhooks({ secret });
+
+// webhooks.on("push", async ({ id, name, payload }) => {
+//   console.log(
+//     `id: ${id}, name: ${name}, ref: ${payload.ref}, head: ${payload.head_commit.id}`
+//   );
+// });
+
+webhooks.onAny(async ({ name }) => {
+  console.log(` name: ${name}, Event erhalten.`);
+});
+
+const app = express();
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(`Received request: ${req.method} ${req.url}`);
+  next();
+});
+const middleware = createNodeMiddleware(webhooks, {
+  path: "/api/github/webhooks",
+});
+app.use(middleware);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
